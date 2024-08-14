a6a7b201db0f2a955a7a088256c58387be25b1fa
darktorres <darktorres@users.noreply.github.com>
2024-08-14 02:13:10 +0000

    deploy: 3c877824472870b130eaea0036b0e341508a47da

--- test/target_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=/home/runner/work/wiRedPanda/Qt/6.6.3/wasm_multithread/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH
-QT_PLUGIN_PATH=/home/runner/work/wiRedPanda/Qt/6.6.3/wasm_multithread/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
-export QT_PLUGIN_PATH
-exec "$@"
